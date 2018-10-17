379881985501f13e42b51c561d19ee603257d4a2
Relliner <41902228+rehlingera@users.noreply.github.com>
2018-10-16 21:44:59 -0500

    most of the game machanics worked out

---
+++ assets/javascript/app.js
@@ -0,0 +1,311 @@
+var trivia = [{
+    question: "Which of these was not the subject of a handheld Disney video game by Tiger?",
+    A: "A Bug's Life",
+    B: "Lion King",
+    C: "Aladdin",
+    D: "Toy Story 2",
+    correct: "D"
+},{
+    question: "Which of these is a system made by Sega?",
+    A: "Master System",
+    B: "64",
+    C: "Genetic",
+    D: "2600",
+    correct: "A"
+},{
+    question: "What is the name of Sonic the Hedgehog's echidna friend?",
+    A: "Tails",
+    B: "Knuckles",
+    C: "Cold Steel",
+    D: "Jeffrey",
+    correct: "B"
+},{
+    question: "If you attack a ____ in Zelda, a whole bunch of them may band together and kill you.",
+    A: "Chicken",
+    B: "Wolf",
+    C: "Cow",
+    D: "Henchman",
+    correct: "A"
+},{
+    question: "What is the name of spaceship on which Master Chief is awakened in the beginning of Halo?",
+    A: "UNSC Warrior",
+    B: "Pillar of Autumn",
+    C: "Shippy McShipface",
+    D: "Voyager",
+    correct: "B"
+},{
+    question: "What is the name of the murderous AI in the game Portal?",
+    A: "jQuery",
+    B: "C3PO",
+    C: "M.O.M.",
+    D: "GladOS",
+    correct: "D"
+},{
+    question: "What is the name of the founder of Aperture Science from the game Portal?",
+    A: "Doug Rattmann",
+    B: "George Costanza",
+    C: "Gordon Freeman",
+    D: "Cave Johnson",
+    correct: "D"
+},{
+    question: "The standard currency for the land of Hyrule from Legend of Zelda is the ____.",
+    A: "Gold",
+    B: "Rupee",
+    C: "Power Gem",
+    D: "Triforce Shard",
+    correct: "B"
+},{
+    question: "In the Legend of Zelda: ____, Link sails among a series of islands to save the world.",
+    A: "Windwaker",
+    B: "Majora's Mask",
+    C: "Of the Caribbean",
+    D: "Sailing Home",
+    correct: "A"
+},{
+    question: "In this game, Nintendo characters get together to fight each other.",
+    A: "Super Mario",
+    B: "Super Smash Bros",
+    C: "Super Ultra Mega",
+    D: "Super Fight Night",
+    correct: "B"
+},{
+    question: "The first video game console available to consumers was the Magnavox Odyssey, released in ____ .",
+    A: "1972",
+    B: "1983",
+    C: "1985",
+    D: "1984",
+    correct: "A"
+},{
+    question: "The first commercially available video game console was called the ____ .",
+    A: "Coleco Vision",
+    B: "Atari 2600",
+    C: "Panasonic F-Box",
+    D: "Magnavox Odyssey",
+    correct: "D"
+},{
+    question: "The Magnavox Odyssey system was created by ____ .",
+    A: "Steve Jobs",
+    B: "Ralph Baer",
+    C: "Bill Gates",
+    D: "Steve Wozniak",
+    correct: "B"
+},{
+    question: "All of the following are games that were available on the original Atari 2600 except:",
+    A: "Adventure",
+    B: "Breakout",
+    C: "Pong",
+    D: "Yar's Revenge",
+    correct: "C"
+},{
+    question: "What is the first planet attacked by the Covenant in the Halo series?",
+    A: "E3-27",
+    B: "Earth",
+    C: "Xagablar",
+    D: "Reach",
+    correct: "D"
+},{
+    question: "In the Halo series, what is the name of the super soldiers fighting for humanity?",
+    A: "Trojans",
+    B: "Spartans",
+    C: "Special Forces",
+    D: "Rangers",
+    correct: "B"
+},{
+    question: "Donkey Kong was acutally called ____ in Japan, but it was mistakenly translated as Donkey Kong.",
+    A: "Monkey Kong",
+    B: "King Kong",
+    C: "Horse Kong",
+    D: "Donkey Bong",
+    correct: "A"
+},{
+    question: "The original Microsoft X-Box was released in ____.",
+    A: "1999",
+    B: "2000",
+    C: "2001",
+    D: "2002",
+    correct: "C"
+},{
+    question: "In 2016, what game won the Game Awards Game of the Year?",
+    A: "Uncharted 4",
+    B: "DOOM",
+    C: "Pokemon Go",
+    D: "Overwatch",
+    correct: "D"
+},{
+    question: "How many characters (including secret) were available in the original Super Smash Bros?",
+    A: "10",
+    B: "12",
+    C: "20",
+    D: "8",
+    correct: "B"
+},{
+    question: "All the following characters were available in Mario Tennis for Nintendo 64 except:",
+    A: "Birdo",
+    B: "Rosalina",
+    C: "Boo",
+    D: "Baby Mario",
+    correct: "B"
+},{
+    question: "All of these characters were available for Mario Kart for Nintendo 64 except:",
+    A: "Bowser",
+    B: "Boo",
+    C: "Toad",
+    D: "Donkey Kong",
+    correct: "B"
+},{
+    question: "The name of the villain in the Sonic the Hedgehog series is called ____.",
+    A: "Knuckles",
+    B: "Dr. Robotnick",
+    C: "Mr. Roboto",
+    D: "Doctor Claw",
+    correct: "B"
+},{
+    question: "The many prizes available to players in the game Smash TV included:",
+    A: "Aquariums",
+    B: "Televisions",
+    C: "Steering Wheels",
+    D: "Silver Nuggets",
+    correct: "B"
+},{
+    question: "Yoshi is what type of animal?",
+    A: "Gecko",
+    B: "Dinosaur",
+    C: "Chameleon",
+    D: "Lizard",
+    correct: "B"
+},{
+    question: "Mario's occupation is ____.",
+    A: "Hero",
+    B: "Warrior",
+    C: "Garbage Man",
+    D: "Plumber",
+    correct: "D"
+},{
+    question: "Which of the following is a fighting game?",
+    A: "Smash Lords",
+    B: "Soulcalibur",
+    C: "Ultimate Punch Face",
+    D: "Boxing Insanity",
+    correct: "B"
+},{
+    question: "In the arcade game ''Toobin','' which of the following is not a risk to the tubers?",
+    A: "Sticks",
+    B: "Rocks",
+    C: "Soda Cans",
+    D: "Bears",
+    correct: "D"
+},{
+    question: "The first item Link obtains in the original Legend of Zelda is a ____.",
+    A: "Lantern",
+    B: "Bow",
+    C: "Sword",
+    D: "Hat",
+    correct: "C"
+},{
+    question: "The name of the main character in the Mass Effect video game series is:",
+    A: "Master Chief",
+    B: "Uhura",
+    C: "Shepherd",
+    D: "Cortana",
+    correct: "C"
+}];
+
+
+window.onload = function () {
+
+    // function getRandomInt(array) {
+    //     return Math.floor(Math.random() * Math.floor(array));
+    // };
+
+    // var triviaQuestion = trivia[getRandomInt(trivia.length)];
+    var intervalId;
+    var timerRunning = true;
+    var usedQuestions = [];
+
+    var newQuestion = function () {
+        function getRandomInt(array) {
+            return Math.floor(Math.random() * Math.floor(array));
+        };
+        var timeLeft = 15;
+        var triviaQuestion = trivia[getRandomInt(trivia.length)];
+        $("#triviaCard").html("<div class='row'><div class='col-md-12' id='questionArea'><p>Question question question question question question question question?</p></div></div><div class='row'><div class='col-md-12'><p style='font-size:14px'>Time left <span id='timeLeft'>00</span> seconds</p></div></div><div class='row'><div class='col-md-12' id='answerA'><p class='answer' id='answerAText' style='text-align:left'><span id='selectA'>► </span>A. Answer</p></div></div><div class='row'><div class='col-md-12' id='answerB'><p class='answer' id='answerBText' style='text-align:left'><span id='selectB'>► </span>B. Answer</p></div></div><div class='row'><div class='col-md-12' id='answerC'><p class='answer' id='answerCText' style='text-align:left'><span id='selectC'>► </span>C. Answer</p></div></div><div class='row' style='padding-bottom: 35%'><div class='col-md-12' id='answerD'><p class='answer' id='answerDText' style='text-align:left'><span id='selectD'>► </span>D. Answer</p></div></div>");
+        $("#timeLeft").html(timeLeft);
+        $("#questionArea").html("<p>" + triviaQuestion.question + "</p>");
+        $("#answerA").html("<p class='answer' id='A' style='text-align:left'><span id='selectA'>► </span>A. " + triviaQuestion.A + "</p>");
+        $("#answerB").html("<p class='answer' id='B' style='text-align:left'><span id='selectB'>► </span>B. " + triviaQuestion.B + "</p>");
+        $("#answerC").html("<p class='answer' id='C' style='text-align:left'><span id='selectC'>► </span>C. " + triviaQuestion.C + "</p>");
+        $("#answerD").html("<p class='answer' id='D' style='text-align:left'><span id='selectD'>► </span>D. " + triviaQuestion.D + "</p>");
+        console.log (triviaQuestion.question);
+        if (timerRunning) {
+            count = function() {
+                timeLeft--;
+                $("#timeLeft").html(timeLeft);
+    
+                if (timeLeft===0) {
+                    clearInterval(intervalId);
+                    outOfTime();
+                };
+            };
+    
+            intervalId = setInterval(count,1000);
+            
+    
+            
+        };
+        
+        var answerPicked = function() {
+            var letter = this.id;
+            console.log (letter);
+            clearInterval(intervalId);
+            if(letter === triviaQuestion.correct) {
+                correctAnswer();
+            }
+            else {
+                wrongAnswer();
+            };
+
+            // if(timeleft === 0) {
+            //     outOfTime();
+            // }
+        };
+    
+        var correctAnswer = function() {
+            console.log("correct");
+            $("#triviaCard").empty();
+            $("#triviaCard").html("<div class='row'><div class='col-md-12'><p style='height:500px'>CORRECT</p></div></div>");
+            setTimeout(function() {
+                $("#triviaCard").empty();
+                newQuestion();
+            },2000);
+        };
+        
+        var wrongAnswer = function() {
+            console.log("wrong");
+            $("#triviaCard").empty();
+            $("#triviaCard").html("<div class='row'><div class='col-md-12'><p>WRONG</p><p style='height:500px'>The correct answer was ''"+triviaQuestion.correct+".''</p></div></div>");
+            setTimeout(function() {
+                $("#triviaCard").empty();
+                newQuestion();
+            },2000);
+        };
+
+        var outOfTime = function() {
+            console.log("out of time");
+            $("#triviaCard").empty();
+            $("#triviaCard").html("<div class='row'><div class='col-md-12'><p style='height:500px'>OUT OF TIME</p></div></div>");
+            setTimeout(function() {
+                $("#triviaCard").empty();
+                newQuestion();
+            },2000);
+        };
+    
+        $(".answer").on("click", answerPicked);
+        };
+
+    newQuestion();
+
+
+
+
+
+};
